a277f7617265dcc6f91e353ce1090b6f25d9fa1c
hatelove <jay.hatelove@gmail.com>
2017-02-06 21:11:45 +0800

    initialized

---
+++ NearBy/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NearBy
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Ignore]
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var numbers = new List<int> {1,2,3,2,1};
+            var distance = 2;
+
+            Assert.IsTrue(Kata.HasNearBy(numbers, distance));
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void TestHasNearBy_false()
+        {
+            var numbers = new List<int> {1, 2, 3, 1, 2};
+            var distance = 2;
+
+            Assert.IsFalse(Kata.HasNearBy(numbers, distance));
+        }
+    }
+
+    public static class Kata
+    {
+        public static bool HasNearBy(List<int> numbers, int distance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
