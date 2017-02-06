c0162f8582a6cce0b045b1cd35dc85d5b72a8ceb
hatelove <jay.hatelove@gmail.com>
2017-02-06 21:21:22 +0800

    distance == 0 or numbers.Count < 2 should return false

--- NearBy/UnitTest1.cs
+++ NearBy/UnitTest1.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace NearBy
 {
@@ -11,7 +10,7 @@ namespace NearBy
         [TestMethod]
         public void TestMethod1()
         {
-            var numbers = new List<int> {1,2,3,2,1};
+            var numbers = new List<int> { 1, 2, 3, 2, 1 };
             var distance = 2;
 
             Assert.IsTrue(Kata.HasNearBy(numbers, distance));
@@ -21,7 +20,7 @@ namespace NearBy
         [TestMethod]
         public void TestHasNearBy_false()
         {
-            var numbers = new List<int> {1, 2, 3, 1, 2};
+            var numbers = new List<int> { 1, 2, 3, 1, 2 };
             var distance = 2;
 
             Assert.IsFalse(Kata.HasNearBy(numbers, distance));
@@ -34,13 +33,26 @@ namespace NearBy
             var distance = 0;
             Assert.IsFalse(Kata.HasNearBy(numbers, distance));
         }
+
+        [TestMethod]
+        public void Test_numbers_length_is_1_should_return_false()
+        {
+            var numbers = new List<int> { 1 };
+            var distance = 1;
+            Assert.IsFalse(Kata.HasNearBy(numbers, distance));
+        }
     }
 
     public static class Kata
     {
         public static bool HasNearBy(List<int> numbers, int distance)
         {
-            throw new NotImplementedException();
+            if (distance == 0 || numbers.Count < 2)
+            {
+                return false;
+            }
+
+            return false;
         }
     }
 }
